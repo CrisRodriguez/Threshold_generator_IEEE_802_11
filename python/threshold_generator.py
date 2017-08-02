fb7dac42d2fb9838597d219859522d5d646d40a7
CrisRodriguez <cristian.rodriguez.ud@gmail.com>
2017-08-02 01:08:37 -0500

    Update threshold_generator.py

--- python/threshold_generator.py
+++ python/threshold_generator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 
 # Copyright 2016 Cristian Rodríguez
-# 	
+# Research group GRECO, Universidad Distrital Francisco Jose de Caldas, Bogota, Colombia	
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
